7ffffa756dcaf26e656901bcebe755147c67a353
Nitin <nitinkmr.4169@gmail.com>
2026-04-18 12:42:28 +0530

    string to uppercase

---
+++ 17.py
@@ -0,0 +1,15 @@
+#17. Write a python pgm to convert a string to uppercase
+string = input("Enter the string: ")
+ans = string.upper()
+print(ans)
+
+#Manual way
+# s = input("Enter the string: ")
+
+# ans = ""
+# for ch in s:
+#   if ch.lower():
+#     ans += chr(ord(ch) - 32)
+#   else:
+#     ans += ch
+# print(ans)      
