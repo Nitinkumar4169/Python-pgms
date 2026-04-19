2a9676b163bc3a12bb3b15a6ac1e7a7531eaf287
Nitin <nitinkmr.4169@gmail.com>
2026-04-19 23:04:16 +0530

    append a text to file from 47.py to 1.py file

--- 1.py
+++ 1.py
@@ -1,2 +1,3 @@
 #The content isHello this is new content
 print("Hello world!")
+#Kya bhai coding kaisi chal rhi hai!!! // Here, I had appended a text in 1.py from 47.py file
---
+++ 47.py
@@ -0,0 +1,5 @@
+#47.Write a python pgm to append text to file
+with open("1.py",'a') as f:
+  data = f.write("Kya bhai coding kaisi chal rhi hai!!!")
+  print(data)
+
