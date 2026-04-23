fff9161cca7042037fb1909113df0b01649aa28b
Nitin <nitinkmr.4169@gmail.com>
2026-04-23 11:01:08 +0530

    update values in a dictionary

---
+++ 93.py
@@ -0,0 +1,6 @@
+#93.Write a python pgm to update values in a dictionary
+d1 = {101:"Apple",201:"Ball",301:"cat",401:"Dog"}
+d2 = {201:"Billu"}
+print("The d1 is: ",d1)
+d1.update(d2)
+print("The updated values in d1 is: ",d1)
