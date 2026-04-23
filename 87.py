0d32833636b1688242e50a8a719b7da5258db579
Nitin <nitinkmr.4169@gmail.com>
2026-04-23 10:17:22 +0530

    find the difference of two sets

---
+++ 87.py
@@ -0,0 +1,6 @@
+#87 Write a py pgm to find the difference of two sets
+s1 = {10, 20, 30, 40}
+print("set s1 =", s1)
+s2 = {40, 50, 60, 70}
+print("set s2 =", s2)
+print("The difference of two sets s1 and s2 are: ", s1.difference(s2))
