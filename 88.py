caa33e37d6aeac9d9fcf13196e295d996efa58f2
Nitin <nitinkmr.4169@gmail.com>
2026-04-23 10:22:56 +0530

    check if a set is subset of another set

---
+++ 88.py
@@ -0,0 +1,7 @@
+#88.Write a py pgm to check if a set is subset of another set
+s1 = {10, 20, 30}
+print("set s1 =", s1)
+s2 = {10, 20, 30, 40}
+print("set s2 =", s2)
+print("The subset of two sets s1 and s2 are: ", s1.issubset(s2))
+#print(s1 <= s2)
