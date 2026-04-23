9977aba183debbef8ccec493df62b63ae06c4b74
Nitin <nitinkmr.4169@gmail.com>
2026-04-23 11:10:42 +0530

    merge two two dictionaries

---
+++ 95.py
@@ -0,0 +1,6 @@
+#95.write a python pgm to merge two two dictionaries
+d1 = {101:"Apple",201:"Ball"}
+print("d1: ",d1)
+d2 = {301:"Cat",401:"Dog"}
+print("d2: ",d2)
+print("Merge of two list is: ",{**d1, **d2})
