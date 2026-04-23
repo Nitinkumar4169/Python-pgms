ef3725b5aa1c258909bf06e2120631183c4e66ba
Nitin <nitinkmr.4169@gmail.com>
2026-04-23 10:55:38 +0530

    create a dictionary of student names and marks

---
+++ 92.py
@@ -0,0 +1,12 @@
+#92. Wap to create a dictionary of student names and marks
+d = {
+  'Nitin': 76,
+  'Nischal': 74,
+  'Pawan': 81,
+  'Sachin': 99,
+  'Rajnish': 89
+  }
+
+print(d)
+for key,values in d.items():
+  print(key,'--->',values)
