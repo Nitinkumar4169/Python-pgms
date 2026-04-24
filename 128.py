473ba5f67040b97cbecfb0d600fc80fb0c529403
Nitin <nitinkmr.4169@gmail.com>
2026-04-24 22:52:26 +0530

    check if a character is uppercase

---
+++ 128.py
@@ -0,0 +1,7 @@
+#128.wap to check if a character is uppercase
+ch = input("Enter a character: ")
+
+if ch.isupper():
+    print("It is an uppercase character")
+else:
+    print("It is not an uppercase character")
