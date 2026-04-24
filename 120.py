0646d02db51073acde40ecf552ebb144d2063b37
Nitin <nitinkmr.4169@gmail.com>
2026-04-24 22:20:53 +0530

    perimeter of a rectangle

---
+++ 120.py
@@ -0,0 +1,5 @@
+#120.wap to calculate perimeter of a rectangle
+
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+print("The perimeter of a rectanle is: ", 2*(length*breadth))
