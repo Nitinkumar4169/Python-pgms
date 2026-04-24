fd7207516682d96f224995dff6abc395647536ef
Nitin <nitinkmr.4169@gmail.com>
2026-04-24 22:10:51 +0530

    area of a rectangle

---
+++ 117.py
@@ -0,0 +1,4 @@
+#117. wap to calculate area of a rectangle
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+print("The area of a rectanle is: ", length*breadth)
