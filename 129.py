cffe4f4eef30ab5c53e4bc663cd5f8a78ecf3787
Nitin <nitinkmr.4169@gmail.com>
2026-04-24 22:53:57 +0530

    check if a character is lowercase

---
+++ 129.py
@@ -0,0 +1,7 @@
+#128.wap to check if a character is lowercase
+ch = input("Enter a character: ")
+
+if ch.islower():
+    print("It is an lowercase character")
+else:
+    print("It is not an lowercase character")
