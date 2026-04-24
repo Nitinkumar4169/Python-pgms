2a8b2b7f28298c8fb11ba1a5ca541dc7e6d6db19
Nitin <nitinkmr.4169@gmail.com>
2026-04-24 12:18:04 +0530

    count the number of words in a sentence

---
+++ 109.py
@@ -0,0 +1,7 @@
+#109.write a python pgm to count the number of words in a sentence
+s = input("Enter the sentence: ").split()
+count = 0
+for i in s:
+  count += 1
+
+print("Number of words in a sentence is: "count)  
