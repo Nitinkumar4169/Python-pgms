fd8b1fe44af2089edddff0f731b75e35ee6f6a90
Nitin <nitinkmr.4169@gmail.com>
2026-04-25 10:56:11 +0530

    calculate power using 'pow()' function

---
+++ 144.py
@@ -0,0 +1,5 @@
+#144.Write a python pgm to calculate power using 'pow()' function
+b = int(input("Enter the base: "))
+p = int(input("Enter the power: "))
+ans = pow(b,p)
+print("The power of a number is: ", ans)
