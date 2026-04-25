2ce4cd8b171cfa561a05032036593860c0fa19e7
Nitin <nitinkmr.4169@gmail.com>
2026-04-25 09:39:34 +0530

    check if a character is an alphabet

---
+++ 131.py
@@ -0,0 +1,6 @@
+#131. Write a python to check if a character is an alphabet
+ch = input("Enter the string: ")
+if ch.isalpha():
+  print("It is an alphabet")
+else:
+  print("It is not an alphabet")  
