f1b50aa0bcf81d4e95f44f18d3a4a6c815f258b8
Nitin <nitinkmr.4169@gmail.com>
2026-04-25 10:49:03 +0530

    find the square root of a number

---
+++ 142.py
@@ -0,0 +1,5 @@
+#142.Write a python pgm to find the square the root of a number
+num = int(input("Enter the number: "))
+ans = num**0.5
+#ans = math.sqrt(num)
+print("The square root of a number is: ",ans)
