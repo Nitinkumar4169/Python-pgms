5b1a9f5aa2c83e1cf84800c764782249b018a86a
Nitin <nitinkmr.4169@gmail.com>
2026-04-26 12:32:04 +0530

    check if a tuple is empty

---
+++ 163.py
@@ -0,0 +1,6 @@
+#163.Write a python pgm to check if a tuple is empty
+tple = tuple(input("Enter the elements: ").split())
+if not tple:
+  print("Tuple is empty")
+else:
+  print("Tuple is not empty")  
