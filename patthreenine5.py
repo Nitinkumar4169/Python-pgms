703bcff1b2ac7e77c55e422697f9f3797a1404e7
Nitin <nitinkmr.4169@gmail.com>
2026-04-30 00:25:47 +0530

    pattern pgm 39 question 5th

--- patthreenine5.py
+++ patthreenine5.py
@@ -1,11 +1,11 @@
 #n = int(input("Enter the number: "))
 
 for i in range(1,5):
-  for outerK in range(5-i-1):
+  for outerK in range(5-i):
     print(" ", end =" ")
 
   if i==1:
-    print("1")
+    print(i)
   else:
     print(i,end=" ")
     for innerK in range(2*i-3):
@@ -14,13 +14,13 @@ for i in range(1,5):
 
 
 
-for i in range(1,6):
-  for outerK in range(i):
+for i in range(1,5+1):
+  for outerK in range(i-1):
     print(" ",end = " ")
 
   
   if i == 5:
-    print(i,end=" ")
+    print(i)
   else:
     print(i,end=" ")
     for innerK in range(2*(5-i)-1):
