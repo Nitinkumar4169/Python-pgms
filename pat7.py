cbb680c2013109293cf04cfecf86f6d1c290e0a1
Nitin <nitinkmr.4169@gmail.com>
2026-04-29 23:35:49 +0530

    pattern printing 39 7th question

---
+++ pat7.py
@@ -0,0 +1,16 @@
+for i in range(1,6):
+  for k in range(6-i):
+    print(" ",end=" ")
+
+    
+  for j in range(i):
+    print(i,end=" ")
+  print()  
+
+for i in range(1,5):
+  for k in range(i+1):
+   print(" ",end=" ")
+
+  for j in range(4-i+1):
+    print(5-i,end=" ")
+  print()  
