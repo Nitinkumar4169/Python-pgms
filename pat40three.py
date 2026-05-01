f78c73569758b0decb44b1feeba54b179224c54c
Nitin <nitinkmr.4169@gmail.com>
2026-05-01 23:13:18 +0530

    triangle pattern in alphabet form

---
+++ pat40three.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for k in range(i):
+        print(' ',end=' ')
+        
+    for j in range(2*(5-i)+1):
+        print(chr(65+j),end=' ')
+    print()    
+            
+        
+ 
