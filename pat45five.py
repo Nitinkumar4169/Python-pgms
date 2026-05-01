8cfe12f218210afa2144eddee5a84bff640ec12a
Nitin <nitinkmr.4169@gmail.com>
2026-05-01 23:19:14 +0530

    pattern printing triangle form 1....9

---
+++ pat45five.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for k in range(i):
+        print(' ',end=' ')
+        
+    for j in range(2*(5-i)+1):
+        print(j+1,end=' ')
+    print()    
+            
+        
+ 
