0c8bc58b1b2a8fc687106412ff3668b53087923f
Nitin <nitinkmr.4169@gmail.com>
2026-05-01 22:51:59 +0530

    triangle pattern , print j

---
+++ pat40two.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for k in range(6-i):
+        print(' ',end=' ')
+        
+    for j in range(2*i-1):
+        print(j+1,end=' ')
+    print()    
+            
+        
+ 
